ff1f9791793d1ea9e270c32f21d1731514fa49f6
Monolith <DARKGREATTHING@outlook.com>
2024-11-26 23:55:59 +0800

    备份

---
+++ Assets/Scripts/AdaptableDialogAnalyzer/View/BanGDream/View_BanGDream_OMCPercent_Item.cs
@@ -0,0 +1,67 @@
+﻿using AdaptableDialogAnalyzer.DataStructures;
+using AdaptableDialogAnalyzer.Unity;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AdaptableDialogAnalyzer.View.BanGDream
+{
+    public class View_BanGDream_OMCPercent_Item : MonoBehaviour
+    {
+        [Header("Components")]
+        public CanvasGroup canvasGroup;
+        public UIFollower uIFollower;
+        public Text txtRank;
+        public Image imgSpeaker;
+        public Image imgAreaShadow;
+        public IndividualColorElement iceBGColor;
+        public IndividualColorElement iceTextColor;
+        public Text txtPercent;
+        public Text txtCount;
+        [Header("Settings")]
+        public IndexedSpriteList charIconList;
+        public IndexedColorList bgColorList;
+        public IndexedColorList textColorList;
+        public float areaShadowAlpha = 0.5f;
+        public float fadeDuration = 0.5f;
+        [Header("Effect")]
+        public View_BanGDream_ItemEffect fadeInEffect;
+        public IndexedHDRColorList hdrColorList;
+
+        View_BanGDream_ItemEffect itemEffect;
+        protected View_BanGDream_ItemEffect ItemEffect => itemEffect;
+
+        public void SetData(SimpleMentionCountResultItemWithRank countResultItem)
+        {
+            CharacterDefinition characterDefinition = GlobalConfig.CharacterDefinition;
+
+            txtRank.text = countResultItem.percentRank.ToString();
+
+            imgSpeaker.sprite = charIconList[countResultItem.characterID];
+            iceBGColor.SetIndividualColor(bgColorList[countResultItem.characterID]);
+            iceTextColor.SetIndividualColor(textColorList[countResultItem.characterID]);
+
+            Color shadowColor = characterDefinition[countResultItem.characterID].color;
+            shadowColor.a = areaShadowAlpha;
+            imgAreaShadow.color = shadowColor;
+
+            txtPercent.text = $"{countResultItem.Percent * 100:00.00}%";
+            txtCount.text = $"{countResultItem.count} / ( {countResultItem.serifCount} )";
+
+            ItemEffect.materialController.HDRColor = hdrColorList[countResultItem.characterID];
+            canvasGroup.alpha = 0;
+        }
+
+        public void Initialize(Transform effectTransform)
+        {
+            itemEffect = Instantiate(fadeInEffect, effectTransform);
+            uIFollower.TargetTransform = itemEffect.transform;
+        }
+
+        public void FadeIn()
+        {
+            itemEffect.particle.Play();
+            canvasGroup.DOFade(1, fadeDuration);
+        }
+    }
+}
